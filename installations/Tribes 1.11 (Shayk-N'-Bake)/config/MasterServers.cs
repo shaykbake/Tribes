e5f40f1222644439e89ca72bef1d5276461df1a0
Shayk-N'-Bake <101651000+shaykbake@users.noreply.github.com>
2022-08-10 12:03:46 -0400

    Add files via upload

---
+++ installations/Tribes 1.11 (Shayk-N'-Bake)/config/MasterServers.cs
@@ -0,0 +1,16 @@
+deleteVariables("$Server::MasterAddress*");
+
+%i = -1;
+$MasterAddress[%i++] = "t1m1.masters.tribesmasterserver.com:28000";
+$MasterAddress[%i++] = "t1m1.pu.net:28000";
+$MasterAddress[%i++] = "t1m1.tribes0.com:28000";
+$MasterAddress[%i++] = "skbmaster.ath.cx:28000";
+$MasterAddress[%i++] = "kigen.ath.cx:28000";
+$MasterAddress[%i++] = "t1m1.masters.dynamix.com:28000";
+
+for(%i = 0; (%address = $MasterAddress[%i]) != ""; %i++)
+{
+	
+	if($Server::MasterAddressN0 == "") $Server::MasterAddressN0 = %address;
+	else $Server::MasterAddressN0 = $Server::MasterAddressN0 @ " " @ %address;
+}
